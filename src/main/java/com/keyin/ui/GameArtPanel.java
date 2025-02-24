20eca2e4571f45d3736bad27ae088627b0ad6681
kyhol <Kyle.Hollett@keyin.com>
2025-02-24 17:52:37 -0330

    NSFW Aciiart removed.

--- src/main/java/com/keyin/ui/GameArtPanel.java
+++ src/main/java/com/keyin/ui/GameArtPanel.java
@@ -21,61 +21,51 @@ public class GameArtPanel extends JPanel {
         artArea.setEditable(false);
 
         String asciiArt =
-            "         .m.                                   ,_\n" +
-                    "         ' ;M;                                ,;m `\n" +
-                    "           ;M;.           ,      ,           ;SMM;\n" +
-                    "          ;;Mm;         ,;  ____  ;,         ;SMM;\n" +
-                    "         ;;;MM;        ; (.MMMMMM.) ;       ,SSMM;;\n" +
-                    "       ,;;;mMp'        l  ';mmmm;/  j       SSSMM;;\n" +
-                    "     .;;;;;MM;         .\\,.mmSSSm,,/,      ,SSSMM;;;\n" +
-                    "    ;;;;;;mMM;        .;MMmSSSSSSSmMm;     ;MSSMM;;;;\n" +
-                    "   ;;;;;;mMSM;     ,_ ;MMmS;;;;;;mmmM;  -,;MMMMMMm;;;;\n" +
-                    "  ;;;;;;;MMSMM;     \\\"*;M;( ( '') );m;*\"/ ;MMMMMM;;;;;,\n" +
-                    " .;;;;;;mMMSMM;      \\(@;! _     _ !;@)/ ;MMMMMMMM;;;;;,\n" +
-                    " ;;;;;;;MMSSSM;       ;,;.*o*> <*o*.;m; ;MMMMMMMMM;;;;;;,\n" +
-                    ".;;;;;;;MMSSSMM;     ;Mm;           ;M;,MMMMMMMMMMm;;;;;;.\n" +
-                    ";;;;;;;mmMSSSMMMM,   ;Mm;,   '-    ,;M;MMMMMMMSMMMMm;;;;;;;\n" +
-                    ";;;;;;;MMMSSSMMMMMMMm;Mm;;,  ___  ,;SmM;MMMMMMSSMMMM;;;;;;;;\n" +
-                    ";;'\";;;MMMSSSSMMMMMM;MMmS;;,  \"  ,;SmMM;MMMMMMSSMMMM;;;;;;;;.\n" +
-                    "!   ;;;MMMSSSSSMMMMM;MMMmSS;;._.;;SSmMM;MMMMMMSSMMMM;;;;;;;;;\n" +
-                    "    ;;;;*MSSSSSSMMMP;Mm*\"'q;'   `;p*\"*M;MMMMMSSSSMMM;;;;;;;;;\n" +
-                    "    ';;;  ;SS*SSM*M;M;'     `-.        ;;MMMMSSSSSMM;;;;;;;;;,\n" +
-                    "     ;;;. ;P  `q; qMM.                 ';MMMMSSSSSMp' ';;;;;;;\n" +
-                    "     ;;;; ',    ; .mm!     \\.   `.   /  ;MMM' `qSS'    ';;;;;;\n" +
-                    "     ';;;       ' mmS';     ;     ,  `. ;'M'   `S       ';;;;;\n" +
-                    "      `;;.        mS;;`;    ;     ;    ;M,!     '  luk   ';;;;\n" +
-                    "       ';;       .mS;;, ;   '. o  ;   oMM;                ;;;;\n" +
-                    "        ';;      MMmS;; `,   ;._.' -_.'MM;                 ;;;\n" +
-                    "         `;;     MMmS;;; ;   ;      ;  MM;                 ;;;\n" +
-                    "           `'.   'MMmS;; `;) ',    .' ,M;'                 ;;;\n" +
-                    "              \\    '' ''; ;   ;    ;  ;'                   ;;\n" +
-                    "               ;        ; `,  ;    ;  ;                   ;;\n" +
-                    "                        |. ;  ; (. ;  ;      _.-.         ;;\n" +
-                    "           .-----..__  /   ;  ;   ;' ;\\  _.-\" .- `.      ;;\n" +
-                    "         ;' ___      `*;   `; ';  ;  ; ;'  .-'    :      ;\n" +
-                    "         ;     \"\"\"*-.   `.  ;  ;  ;  ; ' ,'      /       |\n" +
-                    "         ',          `-_    (.--',`--'..'      .'        ',\n" +
-                    "           `-_          `*-._'.\\\\\\;||\\\\)     ,'\n" +
-                    "              `\"*-._        \"*`-ll_ll'l    ,'\n" +
-                    "                 ,==;*-._           \"-.  .'\n" +
-                    "              _-'    \"*-=`*;-._        ;'\n" +
-                    "            .\"            ;'  ;\"*-.    `\n" +
-                    "            ;   ____      ;//'     \"-   `,\n" +
-                    "            `+   .-/                 \".\\\\;\n" +
-                    "              `*\" /                    \"'";
+                "       .-\"\"-.                     .-\"\"-.          \n" +
+                        "     .'_.-.  |                   |  .-._'.        \n" +
+                        "    /    _/ /       _______       \\ \\_    \\       \n" +
+                        "   /.--.' | |      `=======`      | | '.--.\\\n" +
+                        "  /   .-`-| |       _.---._       | |-`-.   \\\n" +
+                        " ;.--':   | |     .'     / '.     | |   :'--.;\n" +
+                        "|    _\\.'-| |    /      _/   \\    | |-'.\\_    |\n" +
+                        ";_.-'/:\\   | |   /   .-'`  '.  \\   | |   :\\'-._;   \n" +
+                        "|   | _:-'\\  \\.'   | ^ _ ^ |   './  /'-:_ |   |    \n" +
+                        ";  .:` '._ \\.'     \\   _   /     \\ / _.' `:.  ;     \n" +
+                        "|-` '-.;_ ./        '.___.'       '.` _;.-' `-|     \n" +
+                        "; / .'\\ | |        _.'   '._        \\'| /'. \\ ;    \n" +
+                        "| .' / `'.|      .' `\"---\"` '.       |'` \\ '. |    \n" +
+                        ";/  /\\_/-`\\      |           |       /-\\_/\\  \\;    \n" +
+                        " |.' .| `; \\     | |       | |      /;` |. '.|     \n" +
+                        " |  / \\.'\\ /\\    | :--\"\"\"--: |    /`_/'./ \\  |    \n" +
+                        "  \\| ; | ; |/)   | |       | |   (\\| : | ; |/      \n" +
+                        "   \\ | ; | //    \\.'-.....-'./    \\\\ | ; | /       \n" +
+                        "    `\\ | |`/    .' / ;|: | \\ '.    |`| | /`        \n" +
+                        "jgs  .-:_/ \\_.-' .' / ' . : '. '.  /`\\_:'          \n" +
+                        "     |  \\```      .'  ;     \\    `:                \n" +
+                        "      \\  \\                   '     `'.             \n" +
+                        "   .--'\\  |  '         '       .      `-._         \n" +
+                        "  /`;--' /_.'          .                  `-.      \n" +
+                        "  |  `--`        /               \\           \\     \n" +
+                        "   \\       .'   '                 '-.        |     \n" +
+                        "    \\   '               '          __\\       |     \n" +
+                        "     '.      .                 _.-'  `)     /      \n" +
+                        "       '-._                _.-' `| .-`   _.'       \n" +
+                        "           `'--....____.--'|     (`  _.-'          \n" +
+                        "                    /  | |  \\     `\"`              \n" +
+                        "                    \\__/ \\__/                      ";
 
-    artArea.setText(asciiArt);
+        artArea.setText(asciiArt);
 
-    // Center the art
-    JPanel centeringPanel = new JPanel(new GridBagLayout());
-    centeringPanel.setBackground(Color.BLACK);
-    centeringPanel.add(artArea);
+        // Center the art
+        JPanel centeringPanel = new JPanel(new GridBagLayout());
+        centeringPanel.setBackground(Color.BLACK);
+        centeringPanel.add(artArea);
 
-    add(centeringPanel, BorderLayout.CENTER);
-}
+        add(centeringPanel, BorderLayout.CENTER);
+    }
 
-// Method to update the art if needed
-public void setArt(String newArt) {
-    artArea.setText(newArt);
-}
+    // Method to update the art if needed
+    public void setArt(String newArt) {
+        artArea.setText(newArt);
+    }
 }
